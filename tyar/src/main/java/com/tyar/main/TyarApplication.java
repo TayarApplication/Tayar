1789266f7a395c49b437ec6b7dc87a4b27083a90
ashraf <prv.ashraf@gmail.com>
2017-09-26 22:46:31 +0200

    add project folder

---
+++ tyar/src/main/java/com/tyar/main/TyarApplication.java
@@ -0,0 +1,12 @@
+package com.tyar.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TyarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TyarApplication.class, args);
+	}
+}
